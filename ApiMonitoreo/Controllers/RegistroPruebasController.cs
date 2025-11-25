7ea6bc458cb5e6d0c0ca13ac1c1a934116114047
caja <caja@MAYOREO>
2025-11-25 12:55:32 -0700

    Refactorización y ajustes en RegistroPruebasController
    Se realizaron los siguientes cambios:
    - Refactorización de la clase `PruebaInyectDTO` y ajustes de formato.
    - Reestructuración de la clase `RegistroPruebasController` para mejorar la legibilidad.
    - Eliminación de la validación de `EstatusCalidad` en el método `Post`.
    - Ajuste en la consulta de pruebas existentes para incluir `SerieId`.
    - Reformateo del método `GetProduct` sin alterar su lógica.
    - Actualización de la URL en `launchSettings.json` de `localhost` a `192.168.1.96`.

--- ApiMonitoreo/Controllers/RegistroPruebasController.cs
+++ ApiMonitoreo/Controllers/RegistroPruebasController.cs
@@ -4,62 +4,67 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ApiMonitoreo.Controllers
 {
-	public class PruebaInyectDTO
-	{
-		public string NoSerie { get; set; }
-		public int IdPrueba { get; set; }
-		public decimal ValorMedido { get; set; }
-	}
+    public class PruebaInyectDTO
+    {
+        public string NoSerie { get; set; }
+        public int IdPrueba { get; set; }
+        public decimal ValorMedido { get; set; }
+    }
 
-	[Route("api/[controller]")]
-	[ApiController]
-	public class RegistroPruebasController : ControllerBase
-	{
-		private readonly MonitoreoContext _context;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistroPruebasController : ControllerBase
+    {
+        private readonly MonitoreoContext _context;
 
-		public RegistroPruebasController(MonitoreoContext context)
-		{
-			_context = context;
-		}
+        public RegistroPruebasController(MonitoreoContext context)
+        {
+            _context = context;
+        }
 
-		[HttpPost("RealizarPrueba")]
-		public async Task<IActionResult> Post([FromBody] PruebaInyectDTO valores)
-		{
-			var serie = await _context.SerieProductos
-				.FirstOrDefaultAsync(s => s.NumeroSerie == valores.NoSerie && s.EstatusCalidad == "Pendiente");
+        [HttpPost("RealizarPrueba")]
+        public async Task<IActionResult> Post([FromBody] PruebaInyectDTO valores)
+        {
+            var serie = await _context.SerieProductos
+                .FirstOrDefaultAsync(s => s.NumeroSerie == valores.NoSerie);
 
-			var pruebaExistente = await _context.HistorialPruebas.FirstOrDefaultAsync(p => p.Idprueba == valores.IdPrueba);
+            var pruebaExistente = await _context.HistorialPruebas
+                .FirstOrDefaultAsync(p =>
+                    p.Idprueba == valores.IdPrueba &&
+                    p.SerieId == serie.SerieId
+                );
 
-			if (serie == null || pruebaExistente != null)
-				return NotFound("El numero de serie proporcionado no se le puede realizar la prueba que solicita");
 
-			var reg = new HistorialPrueba();
-			reg.Idprueba = valores.IdPrueba;
-			reg.SerieId = serie.SerieId;
-			reg.ValorMedido = valores.ValorMedido;
+            if (serie == null || pruebaExistente != null)
+                return NotFound("El numero de serie proporcionado no se le puede realizar la prueba que solicita");
 
-			await _context.HistorialPruebas.AddAsync(reg);
-			await _context.SaveChangesAsync();
+            var reg = new HistorialPrueba();
+            reg.Idprueba = valores.IdPrueba;
+            reg.SerieId = serie.SerieId;
+            reg.ValorMedido = valores.ValorMedido;
 
-			return Ok(new { message = "Prueba registrada correctamente" });
-		}
+            await _context.HistorialPruebas.AddAsync(reg);
+            await _context.SaveChangesAsync();
 
-		[HttpGet("Resultados/{noSerie}")]
-		public async Task<IActionResult> GetProduct(string noSerie)
-		{
-			var result = await  _context.HistorialPruebas
-									.Join(_context.SerieProductos, hp => hp.SerieId, sp => sp.SerieId, (hp, sp) => new { hp, sp })
-									.Where(s=>s.sp.NumeroSerie == noSerie)
-									.Join(_context.Pruebas, x => x.hp.Idprueba, p => p.Idprueba, (x, p) => new { x.hp, x.sp, p })
-									.Join(_context.PruebaProductos, x => x.p.Idprueba, pp => pp.Idprueba, (x, pp) => new
-									{
-										Descripcion = x.p.Descripcion,
-										ValorMedido = x.hp.ValorMedido,
-										ValorEsperado = pp.ValorEsperado,
-										Tolerancia = pp.Tolerancia
-									}).ToListAsync();
+            return Ok(new { message = "Prueba registrada correctamente" });
+        }
 
-			return Ok(result);
-		}
-	}
+        [HttpGet("Resultados/{noSerie}")]
+        public async Task<IActionResult> GetProduct(string noSerie)
+        {
+            var result = await _context.HistorialPruebas
+                                    .Join(_context.SerieProductos, hp => hp.SerieId, sp => sp.SerieId, (hp, sp) => new { hp, sp })
+                                    .Where(s => s.sp.NumeroSerie == noSerie)
+                                    .Join(_context.Pruebas, x => x.hp.Idprueba, p => p.Idprueba, (x, p) => new { x.hp, x.sp, p })
+                                    .Join(_context.PruebaProductos, x => x.p.Idprueba, pp => pp.Idprueba, (x, pp) => new
+                                    {
+                                        Descripcion = x.p.Descripcion,
+                                        ValorMedido = x.hp.ValorMedido,
+                                        ValorEsperado = pp.ValorEsperado,
+                                        Tolerancia = pp.Tolerancia
+                                    }).ToListAsync();
+
+            return Ok(result);
+        }
+    }
 }
